bb1f2263fbbe27daebf2c58d70fc11561d2d8aaf
Mateo Díaz <mateodiaz401@gmail.com>
2023-01-06 15:18:06 -0500

    add ssr fetch

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    formats: ['image/webp', 'image/avif'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/600/**'
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
--- pages/ssr.js
+++ pages/ssr.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import { getRandomPhoto } from '../data/api';
+import Image from 'next/image';
 
-const ssr = () => {
+
+
+const SSR = ({ photo }) => {
     return (
         <div>
-            SSR
+            <h1>{photo.title}</h1>
+            <Image
+                src={photo.url + '.jpg'}
+                alt={photo.title}
+                width={345}
+                height={500}
+                priority
+            />
         </div>
     );
 };
 
-export default ssr;
+export async function getServerSideProps() {
+    let photo;
+    try {
+        photo = await getRandomPhoto();
+    } catch (e) {
+        /**
+         * If something goes wrong, we return a 404 page
+         */
+        return {
+            notFound: true,
+        };
+    }
+
+    if (!photo) {
+        /**
+         * If we don't get a `photo` back, we return a 404 page
+         */
+        return {
+            notFound: true,
+        };
+    }
+
+    return {
+        props: {
+            photo,
+        },
+    };
+}
+
+export default SSR;
